526bc9022ab01c4a00da6d7234753aa99a6db4ac
amyre-codes <52667955+amyre-codes@users.noreply.github.com>
2019-07-18 11:34:28 -0400

    Add files via upload

---
+++ day9/MorningExercise.java
@@ -0,0 +1,32 @@
+package day9;
+
+import java.util.Scanner;
+
+public class MorningExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner man = new Scanner(System.in);
+  //       int orange = 5;
+ //     int black = 10;
+ //      int blue = 15;
+         
+		System.out.println("Welcome to Amyre's Calculator!");
+         System.out.println("enter a number");
+        		int orange = man.nextInt();
+        		 
+      //  		 System.out.println("enter another number");
+    //    		 black = man.nextInt();
+        		 
+        		 for(int x=orange; x<=100; x++) {
+        			 	 
+        		 if(orange>50) { 
+        			 System.out.println(x);
+        			 
+        			 
+        			 
+	}
+	}
+}
+}
