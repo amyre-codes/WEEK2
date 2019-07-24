0cf7b9243b187572864d1eaa9c45772a04666082
amyre-codes <52667955+amyre-codes@users.noreply.github.com>
2019-07-24 11:25:38 -0400

    Update theTest.java
    finished group project.

--- theTest.java
+++ theTest.java
@@ -1,9 +1,5 @@
 package day9;
-
-	
-		
-		
-	import java.util.Scanner;
+import java.util.Scanner;
 
 public class theTest {
 
@@ -27,26 +23,34 @@ public class theTest {
 		food = scam.next();
 		
 		
-		System.out.println("Wow, " + sport + " is an amazing sport that I'm pretty sure you're very good at. I see " + food + " is your favorite food. Good choice, I love" + food + "myself." );
-		
-		int color = 15;
-		int red = 20;
-		int pink = 4;
-		
+		System.out.println("Wow, " + sport + " is an amazing sport that I'm pretty sure you're very good at I see ");
+		System.out.println(food + " is your favorite food good choice, I love " + food + "." );
+		long color = 15;
+		long red = 20;
+		long pink = 4;
+//		long fred = 0;
+  long apple = 10;
+//		fred = scam.nextLong();
 	
+	System.out.println("Enter a number and watch what it do");
+	 apple = scam.nextLong();
+	
+	for(long x= apple ; x<=100; x++) {
+			System.out.println(x);
 		
-		System.out.println("Have anything adding, subtracting, multiplying, or dividing to do?" + name + " We have a calculator on deck for you. Would you like to try it out? Answer with yes or no please." );
-		
+	}
+		System.out.println("Do you have any adding, subtracting, multiplying, or dividing to do? " + name);
+		System.out.println(" We have a calculator, would you like to try it out? Answer with yes or no please.");
 		String answer = "";
 		answer = scam.next();
 		
 		if (answer.equalsIgnoreCase("yes")){
 		
 		System.out.println("Enter a number");
-		color = scam.nextInt();
+		color = scam.nextLong(); 
 		
 		System.out.println("Enter another number");
-		red = scam.nextInt();
+		red = scam.nextLong(); 
 		
 		
 		
@@ -69,27 +73,20 @@ public class theTest {
 		 System.out.println("Thank you for taking our survey and trying our calculator.");
 		 
 		} else {
+			
+		
+			}
+			
 				
-			System.out.println("Ok thank you for your participation.");
-			
-			
+					System.out.println("Ok thank you for your participation.");
 		} 
-	}
+	
 
 	}
-	 
-		 
+	
 		
-		
-				
 	
 		
 		
-		
-				
-		
-		
-		
-		
-
 	
+		
